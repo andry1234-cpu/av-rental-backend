b2e596d3eb6c7e8f572909c8d4514c8f0d33844d
Andrea Ziccardi <andryziccardi@gmail.com>
2025-10-29 00:27:48 +0100

    Aggiunti campi peso, dimensioni e consumo energetico al modello Equipment

--- models/Equipment.js
+++ models/Equipment.js
@@ -4,7 +4,33 @@ const equipmentSchema = new mongoose.Schema({
   name: String,
   category: String,
   quantity: Number,
-  imageUrl: String
+  imageUrl: String,
+  weight: {
+    value: Number,
+    unit: {
+      type: String,
+      enum: ['kg', 'g'],
+      default: 'kg'
+    }
+  },
+  dimensions: {
+    length: Number,
+    width: Number,
+    height: Number,
+    unit: {
+      type: String,
+      enum: ['cm', 'mm', 'm'],
+      default: 'cm'
+    }
+  },
+  powerConsumption: {
+    value: Number,
+    unit: {
+      type: String,
+      enum: ['W', 'kW'],
+      default: 'W'
+    }
+  }
 });
 
 module.exports = mongoose.model('Equipment', equipmentSchema);
