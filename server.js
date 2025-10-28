67b506e604ab1db9ed45d15f8bffb76cbee55997
Andrea Ziccardi <andryziccardi@gmail.com>
2025-10-28 22:32:28 +0100

    Abilita CORS per richieste dal frontend

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 app.use(express.json());
