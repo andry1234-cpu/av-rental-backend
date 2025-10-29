a1f93038bbf2f0279072831dd095b3574c5b8cdd
Andrea Ziccardi <andryziccardi@gmail.com>
2025-10-29 01:15:30 +0100

    Test: verifica auto-deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('✅ Connessione a MongoDB riuscita'))
+.then(() => console.log('✅ Connessione a MongoDB riuscita! [Auto-deploy test]'))
 .catch(err => console.error('❌ Errore MongoDB:', err));
 
 // Modello Equipment
